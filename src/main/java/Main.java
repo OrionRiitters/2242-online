57b3e2080d9fea041d78eae265747e39f6daaef8
Orion McCoy-Riitters <orionriitters@gmail.com>
2019-05-16 05:23:12 -0500

    Added reinitialization to main()'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,31 @@ import statemanager.Scheduler;
 public class Main {
 
     public static void main(String[] args) {
-        TheServer server = new TheServer();
-        server.start();
 
-        Scheduler scheduler = Scheduler.get_instance();
-        scheduler.initializeGame();
-        scheduler.run();
+        try {
+            TheServer server = new TheServer();
+            server.start();
+
+            Scheduler scheduler = Scheduler.get_instance();
+            scheduler.initializeGame();
+            scheduler.run();
+        } catch (Exception e) {
+            Main.reinitialize();
+
+        }
+    }
+
+    private static void reinitialize() {
+        try {
+            TheServer server = new TheServer();
+            server.start();
+
+            Scheduler scheduler = Scheduler.get_instance();
+            scheduler.initializeGame();
+            scheduler.run();
+        } catch (Exception e) {
+            Main.reinitialize();
+        }
 
     }
 
